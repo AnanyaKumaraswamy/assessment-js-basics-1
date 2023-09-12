e22b2ab617bc94c44eb5fd9e98af38af5570cdec
AnanyaKumaraswamy <ananya.tejas19@gmail.com>
2023-09-11 22:59:01 -0500

    Final commit

--- sdlc.js
+++ sdlc.js
@@ -12,6 +12,15 @@
 // Testing & Integration
 // Planning
 
+//Answer: 
+//Software Development Lifecycle :    
+//1.Planning
+//2.Analysis of Requirements 
+//3.Design
+//4.Implementation 
+//5.Testing & Integration
+//6.Maintain
+
 
 //////////////////PROBLEM 2////////////////////
 /*
@@ -19,3 +28,9 @@
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysis = "This is the second step, in which we define product requirements,evaluate and identify resources and do a feasility assessment."
+const design = "This is the third step, which includes High-Level Design (HLD) that provides the architecture of the software product and Low-Level Design (LLD) that explains the functioning of every feature and component in the product and its working."
+const implementation = "This is the fourth step, where the software developers start writing the code for the product, the operations team will set up the physical hardware for the servers, and the designers will program the user interface."
+const testing = "This is the fifth step, where team validates whether all of the requirements have been met, reassures all the functionality is working as expected and identify all the possible bugs and report.Once the product is tested and ready to be deployed it is released formally in the appropriate market. "
+const maintain = "This is the sixth step, where the maintenance team will regularly examine the application, monitor the load on the servers, and more. The maintenance team will support the application in all circumstances and further update and enhance the software."
+
