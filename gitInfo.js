3141459dd47436a08c3e731b30448dd54502a9ed
AnanyaKumaraswamy <ananya.tejas19@gmail.com>
2023-09-12 00:19:31 -0500

    Commit gitinfo

--- gitInfo.js
+++ gitInfo.js
@@ -10,7 +10,8 @@
 */
 
 //CODE HERE
-
+let gitDefinition = "Git is a version control system that allows individuals and teams to track changes to code projects.Prevents bugs and loss of progress and allows us to view the long history of a project in snapshots.";
+console.log('Git:', gitDefinition);
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable called 'gitHubDefinition'.  
@@ -18,7 +19,8 @@
 */
 
 //CODE HERE
-
+let gitHubDefinition = "GitHub is a code hosting platform for version control and collaboration. It lets you and others work together on projects from anywhere.";
+console.log('Git Hub:', gitHubDefinition);
 //////////////////PROBLEM 3////////////////////
 /*
     Create a variable called 'gitInitDefinition'.  
@@ -26,7 +28,8 @@
 */
 
 //CODE HERE
-
+let gitInitDefinition = "The git init command creates a new Git repository. It can be used to convert an existing project to a Git repository or initialize a new, empty repository.";
+console.log('Git-init:', gitInitDefinition);
 //////////////////PROBLEM 4////////////////////
 /*
     Create a variable called 'gitCloneDefinition'.  
@@ -34,7 +37,8 @@
 */
 
 //CODE HERE
-
+let gitCloneDefinition ="The git clone is primarily used to point to an existing repo and make a clone or copy of that repo at in a new directory, at another location."
+console.log('Git-clone:', gitCloneDefinition);
 //////////////////PROBLEM 5////////////////////
 /*
     Create a variable called 'gitStatusDefinition'.  
@@ -42,7 +46,8 @@
 */
 
 //CODE HERE
-
+let gitStatusDefinition ="The git status command displays the state of the working directory and the staging area. It lets you see which changes have been staged, which haven't, and which files aren't being tracked by Git."
+console.log('Git-status:', gitStatusDefinition);
 //////////////////PROBLEM 6////////////////////
 /*
     Create a variable called 'gitAddDefinition'.  
@@ -53,7 +58,11 @@
 */
 
 //CODE HERE
+let gitAddDefinition ="The git add command adds a change in the working directory to the staging area. It tells Git that you want to include updates to a particular file in the next commit."
+console.log('Git-add:', gitAddDefinition);
 
+let gitAddCode = "git add ."
+console.log('Code to add all files: ', gitAddCode);
 //////////////////PROBLEM 7////////////////////
 /*
     Create a variable called 'gitCommitDefinition'.  
@@ -64,7 +73,11 @@
 */
 
 //CODE HERE
+let gitCommitDefinition ="The git commit command creates a commit, which is like a snapshot of your repository. These commits are snapshots of your entire repository at specific times. "
+console.log('Git-commit:', gitCommitDefinition);
 
+let gitCommitCode = "git commit -am 'initial commit' "
+console.log('Code to commit: ', gitCommitCode);
 
 //////////////////PROBLEM 8////////////////////
 /*
@@ -73,3 +86,5 @@
 */
 
 //CODE HERE
+let gitPushDefinition ="The git push command is used to upload local repository content to a remote repository."
+console.log('Git-push:', gitPushDefinition);
